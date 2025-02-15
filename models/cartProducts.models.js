ae88e377c953ba7b51999fdc7d7906a16a7537d9
Suyash Nandurkar <suyashnandurkar53@gmail.com>
2025-02-15 09:42:53 +0530

    trying to fix cart updates

--- models/cartProducts.models.js
+++ models/cartProducts.models.js
@@ -71,8 +71,7 @@ const cartProductSchema = new mongoose.Schema(
     ],
     quantity: {
         type: Number,
-        required: true,
-        default: 0,
+        default: 1,
     }
   },
   { timestamps: true },
