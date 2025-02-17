9154b6062772e112887333a1011d7eb26519fd10
Suyash Nandurkar <suyashnandurkar53@gmail.com>
2025-02-17 21:28:16 +0530

    fixed schema for buying addres

--- models/buyingAddress.models.js
+++ models/buyingAddress.models.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose")
 
 const addressSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
+    name: {
         type: String,
         required: true,
     },
